3a0d34bbf98c1c8687dbd0f5277f56ca52aba170
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-12-13 10:15:27 +0100

    09_BestCategories.sql

---
+++ 2022.02.13/09_BestCategories.sql
@@ -0,0 +1,11 @@
+SELECT 
+    COUNT(p.name) AS items_count,
+    c.name,
+    SUM(p.quantity_in_stock) AS total_quantity
+FROM
+    categories AS c
+        JOIN
+    products AS p ON c.id = p.category_id
+GROUP BY c.name
+ORDER BY items_count DESC , total_quantity
+LIMIT 5;
