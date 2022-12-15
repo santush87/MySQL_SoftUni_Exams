866c914aa338f666bbdbbd7394e7b2d4c2fe6276
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-12-15 10:04:27 +0100

    02_Insert.sql

---
+++ 2022.04.10/02_Insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO actors(first_name, last_name, birthdate, height, awards, country_id)(
+SELECT 
+(reverse(first_name)), 
+(reverse(last_name)), 
+(DATE(birthdate - 2)), 
+(height + 10), 
+(country_id), 
+(3) 
+FROM actors
+WHERE id <= 10
+);
