a4a5385d81383569aaed8ee8ff70305d43071ee7
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2022-12-20 08:14:38 +0100

    07_MovieCasting.sql

---
+++ 2022.04.10/07_MovieCasting.sql
@@ -0,0 +1,15 @@
+SELECT 
+    CONCAT(first_name, ' ', last_name) AS full_name,
+    CONCAT(REVERSE(last_name),
+            CHARACTER_LENGTH(last_name),
+            '@cast.com') AS email,
+    2022 - YEAR(birthdate) AS age,
+    height
+FROM
+    actors
+WHERE
+    id NOT IN (SELECT 
+            actor_id
+        FROM
+            movies_actors)
+ORDER BY height;
