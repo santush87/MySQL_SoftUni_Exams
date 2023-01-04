4ca0ad852ee809123f103258d0daab3620acd37a
Martin Aleksandrov <73018624+santush87@users.noreply.github.com>
2023-01-04 11:20:01 +0200

    09_Availability.sql

---
+++ 2022.10.15/09_Availability.sql
@@ -0,0 +1,21 @@
+SELECT 
+    t.id,
+    t.capacity,
+    COUNT(c.id) AS count_clients,
+    CASE
+        WHEN COUNT(c.id) - t.capacity = 0 THEN 'Full'
+        WHEN COUNT(c.id) - t.capacity > 0 THEN 'Extra seats'
+        ELSE 'Free seats'
+    END AS availability
+FROM
+    `tables` AS t
+        JOIN
+    orders AS o ON t.id = o.table_id
+        JOIN
+    orders_clients AS oc ON o.id = oc.order_id
+        JOIN
+    clients AS c ON c.id = oc.client_id
+WHERE
+    t.floor = 1
+GROUP BY t.id
+ORDER BY t.id DESC
